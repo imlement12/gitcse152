675454998969f5d673741493ba91ea41c432d82b
Lateef5Smart <oladehindelateef1@gmail.com>
2022-08-26 07:17:01 +0100

    i lateef has also edit the implementation

---
+++ lateef project/Dashboard.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <div>
+<div class="paragraph">
+  <h2 class="now">The List of our Available Products</h2>
+</div>
+<section class="my-4">
+</div>
+<div class="container-fluid">
+ <div class="row">
+ <div class="col-lg-4 col-md-4 col-12">
+  <a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~4.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~3.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~2.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~9.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~5.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~7.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~8.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~15.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~16.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~10.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~11.jpg" class="img-fluid pb-3"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<a href="C:\xampp\htdocs\photo\order.php"><img src="images/Class Project UI & UX Design edited~12.jpg" class="img-fluid pb-3"></a>
+</div>
+
+</div>
+</section>
+</div>
+<div>
+    <div class="check">
+        <h2 class="myorder">my order</h2>
+        <P>View All Orders<br>View Order Number/Order/Amount/Quality<br>View 
+        Status (in Process, Shipped, Open, Reject)<br>History/Receipt<br>View Details Of All Order Made<br>
+    View Order id, Status, Date Order<br>Link To Re-order</P><br><br><br>
+    </div>
+    <div class="order">
+        <h2 class="checkout">Check Out</h2>
+        <p>Proceed To Shipping Details<br>View/Edit Shipping Details<br>Select Shipping Method<br>
+        Proceed To Billing Details<br>View/ Edit Billing Details<br>Review Order<br>Total Order Summary,
+        With Shipping and Billing Details<br>Proceed To Payment Details<br>Card Payment Integration
+    <br>Order Confirmation</p>
+    </div>
+    <div class="about">
+        <h2>About US</h2>
+        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laudantium, voluptatem sint nesciunt omnis labore nulla veniam ratione repellat sunt quisquam necessitatibus
+             nisi delectus explicabo earum saepe, hic sit laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis culpa doloremque, dolorum esse assumenda facilis? Vel aut laboriosam dolor 
+             obcaecati excepturi quasi libero odio ab nam vitae modi, perspiciatis nobis!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquid inventore
+              fugit necessitatibus voluptates, minus quos dolorem delectus? Exercitationem, eligendi dolores voluptate
+              modi vero nisi. In corporis facere deserunt tempore.</P>
+    </div>
+    <div class="disclaimer">
+        <h2>Disclaimer</h2>
+        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laudantium, voluptatem sint nesciunt omnis labore nulla veniam ratione repellat sunt quisquam necessitatibus
+             nisi delectus explicabo earum saepe, hic sit laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis culpa doloremque, dolorum esse assumenda facilis? Vel aut laboriosam dolor 
+             obcaecati excepturi quasi libero odio ab nam vitae modi, perspiciatis nobis!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquid inventore
+              fugit necessitatibus voluptates, minus quos dolorem delectus? Exercitationem, eligendi dolores voluptate
+              modi vero nisi. In corporis facere deserunt tempore.</P>
+    </div>
+    <div class="contact">
+        <h2>Contact Us</h2>
+        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laudantium, voluptatem sint nesciunt omnis labore nulla veniam ratione repellat sunt quisquam necessitatibus
+             nisi delectus explicabo earum saepe, hic sit laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis culpa doloremque, dolorum esse assumenda facilis? Vel aut laboriosam dolor 
+             obcaecati excepturi quasi libero odio ab nam vitae modi, perspiciatis nobis!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquid inventore
+              fugit necessitatibus voluptates, minus quos dolorem delectus? Exercitationem, eligendi dolores voluptate
+              modi vero nisi. In corporis facere deserunt tempore. </P>
+    </div>
+    <div class="term">
+        <h2>Terms and Condition</h2>
+        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laudantium, voluptatem sint nesciunt omnis labore nulla veniam ratione repellat sunt quisquam necessitatibus
+             nisi delectus explicabo earum saepe, hic sit laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis culpa doloremque, dolorum esse assumenda facilis? Vel aut laboriosam dolor 
+             obcaecati excepturi quasi libero odio ab nam vitae modi, perspiciatis nobis!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquid inventore
+              fugit necessitatibus voluptates, minus quos dolorem delectus? Exercitationem, eligendi dolores voluptate
+              modi vero nisi. In corporis facere deserunt tempore.</P>
+    </div>
+    <div class="privacy">
+        <h2>Privacy Condition</h2>
+        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laudantium, voluptatem sint nesciunt omnis labore nulla veniam ratione repellat sunt quisquam necessitatibus
+             nisi delectus explicabo earum saepe, hic sit laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis culpa doloremque, dolorum esse assumenda facilis? Vel aut laboriosam dolor 
+             obcaecati excepturi quasi libero odio ab nam vitae modi, perspiciatis nobis!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquid inventore
+              fugit necessitatibus voluptates, minus quos dolorem delectus? Exercitationem, eligendi dolores voluptate
+              modi vero nisi. In corporis facere deserunt tempore.</P>
+    </div>
+    <div class="security">
+        <h2>Security</h2>
+        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laudantium, voluptatem sint nesciunt omnis labore nulla veniam ratione repellat sunt quisquam necessitatibus
+             nisi delectus explicabo earum saepe, hic sit laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis culpa doloremque, dolorum esse assumenda facilis? Vel aut laboriosam dolor 
+             obcaecati excepturi quasi libero odio ab nam vitae modi, perspiciatis nobis!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquid inventore
+              fugit necessitatibus voluptates, minus quos dolorem delectus? Exercitationem, eligendi dolores voluptate
+              modi vero nisi. In corporis facere deserunt tempore.</P>
+    </div>
+   
+</div>
+</div>
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/proper.js/1.16.0/umd/proper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
